bae4c52be90cdb666eeda560b6502ddd82f22806
Anna23041984 <savchenkoanna2304@gmail.com>
2024-06-20 14:41:10 +0200

    dz-2
    dz2 javascript

---
+++ dz_2.js
@@ -0,0 +1,6 @@
+let school = 46;
+let clas = "9B";
+let students = 26;
+
+const dataschool = `Schools data are:  name school - ${school}, class - ${clas}, number students - ${students}.`;
+console.log(dataschool);
