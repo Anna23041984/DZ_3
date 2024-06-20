d7c1df05bf3ba6d8d4d9b447678aec4925e904eb
Anna23041984 <savchenkoanna2304@gmail.com>
2024-06-20 15:57:39 +0200

    Create dz_1.js
    dz-1

---
+++ dz_1.js
@@ -0,0 +1,10 @@
+console.log(typeof 10); //number
+console.log(typeof "Alex"); //string
+console.log(typeof 1>2); // boolean 
+console.log(typeof null);// object
+console.log(typeof abc);//Indefined
+console.log(typeof Uniquesymbol);//Symbol
+console.log(typeof 1234567890123456789012345678901234567890n);//bigInt
+
+
+
