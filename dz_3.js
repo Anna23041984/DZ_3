156a9f9008c80b3b22cd5906bf5d0754aa272b51
Anna23041984 <savchenkoanna2304@gmail.com>
2024-06-20 15:08:31 +0200

    Create dz_3.js
    dz_3

---
+++ dz_3.js
@@ -0,0 +1,10 @@
+//const Number = 10369;
+
+const a = '1';
+const b = '0';
+const c = '3';
+const d = '6';
+const e = '9';
+
+const numberString = a + " " + b + " " + c + " " + d + " " + e;
+console.log (numberString);
